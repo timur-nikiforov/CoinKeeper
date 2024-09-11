46a7cf0ae4979ba57793108c9b99f4bd35af66a3
discovery126 <den4ikus03@mail.ru>
2024-09-11 03:39:06 +0300

    Initial commit

---
+++ src/main/java/org/denis/coinkeeper/CoinkeeperApplication.java
@@ -0,0 +1,14 @@
+package org.denis.coinkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinkeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/denis/coinkeeper/CoinkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.denis.coinkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinkeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
